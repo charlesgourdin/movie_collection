085e294afbb159e3645aa227dac435c401f8d592
Charles Gourdin <gourdin.charles@gmail.com>
2020-12-08 19:45:14 +0100

    [feat] create and discover my first vue component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome Charles, to Your Vue.js App"/> -->
+    <MovieList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import MovieList from './components/MoviesList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    MovieList,
+
   }
 }
 </script>
---
+++ src/components/MoviesList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="movies">
+    {{ movies.title }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'MoviesList',
+  data() {
+    return {
+      movies: {},
+    }
+  },
+  methods: {
+  },
+  async created() {
+    const response = await axios.get('https://api.themoviedb.org/3/movie/550?api_key=49f6cce872dedfb45746781479e03ce5');
+
+    console.log(response.data)
+
+    this.movies = response.data;
+  }
+ };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
